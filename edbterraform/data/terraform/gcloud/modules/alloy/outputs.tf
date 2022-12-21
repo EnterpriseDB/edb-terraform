e3a4ec164224e7249314bee37c278967aba1d1b9
Bryan Barajas <bryan.barajas@enterprisedb.com>
2022-12-21 06:34:24 +0000

    AlloyDB support * database name not an option through terraform * ports not configurable * database version inferred by configuration set up, postgres 14 compatable * only primary set up * use of bastion host in the same region or other method to access alloydb instance * uses google-beta provider

--- edbterraform/data/terraform/gcloud/modules/alloy/outputs.tf
+++ edbterraform/data/terraform/gcloud/modules/alloy/outputs.tf
@@ -1,10 +1,8 @@
 output "ips" {
   value = {
-    region   = var.aurora.spec.region
-    username = aws_rds_cluster.aurora_cluster.master_username
-    password = aws_rds_cluster.aurora_cluster.master_password
-    address  = aws_rds_cluster.aurora_cluster.endpoint
-    port     = var.alloy.spec.port
-    dbname   = var.alloy.spec.dbname
+    region   = var.region
+    password = var.password
+    address  = google_alloydb_instance.main.ip_address
+    port     = var.port
   }
 }
