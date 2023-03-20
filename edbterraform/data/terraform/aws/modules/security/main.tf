97b4fa852bdb599881e5e749620132aa38d4cf48
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-03-20 21:20:50 +0000

    Issue: AWS quota limit hit when using too many security groups
    Causes:
    VPC Security Group limit per instance
    VPC security Groups limit per elastic network interface
    Ref: https://repost.aws/questions/QUragGhbTXSqSKO4B1zIJUGw/quot-exceeded-quot-vpc-security-groups-limit-when-launching-instances-with-one-sg
    
    Fix:
    Use `aws_security_group_rules` with pre-set `aws_security_group`s.
    This is also a workaround for the issue where an `aws_instance` is force recreated when a security group is added or removed.

--- edbterraform/data/terraform/aws/modules/security/main.tf
+++ edbterraform/data/terraform/aws/modules/security/main.tf
@@ -1,40 +1,37 @@
 resource "aws_security_group" "rules" {
+  name = format("%s_%s", var.project_tag, var.cluster_name)
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = format("%s_%s", var.project_tag, var.cluster_name)
+  }
+}
+
+resource "aws_security_group_rule" "ingress" {
   for_each = {
     # preserve ordering
     for index, values in var.ports:
       format("0%.3d",index) => values
   }
-  name = format("%s_%s_%s_%s", var.project_tag, var.cluster_name, each.value.protocol, each.key)
+  security_group_id = aws_security_group.rules.id
   description = each.value.description
-  vpc_id = var.vpc_id
-  ingress {
-      from_port   = each.value.protocol == "icmp" ? 8 : each.value.port != null ? each.value.port : -1
-      to_port     = each.value.port != null && each.value.protocol != "icmp"  ? each.value.port : -1
-      protocol    = each.value.protocol
-      cidr_blocks = each.value.ingress_cidrs != null ? each.value.ingress_cidrs : var.ingress_cidrs
-  }
+  type = "ingress"
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = each.value.egress_cidrs != null ? each.value.egress_cidrs : var.egress_cidrs
-  }
-
-  tags = {
-    Name = format("%s_%s_%s_%s", var.project_tag, var.cluster_name, each.value.protocol, each.key)
-  }
+  from_port   = each.value.protocol == "icmp" ? 8 : each.value.port != null ? each.value.port : -1
+  to_port     = each.value.port != null && each.value.protocol != "icmp"  ? each.value.port : -1
+  protocol    = each.value.protocol
+  cidr_blocks = each.value.ingress_cidrs != null ? each.value.ingress_cidrs : var.ingress_cidrs
 }
 
 # Default is removed from VPC when this resource is used
 # Manually add it back to allow instances to have outbound access
 # Should be moved to network tags so machines can specify if they need public access
-resource "aws_security_group" "OUTBOUND_ACCESS" {
-  vpc_id = var.vpc_id
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "OUTBOUND_ACCESS" {
+  security_group_id = aws_security_group.rules.id
+  description = "OUTBOUND access"
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = -1
+  cidr_blocks = ["0.0.0.0/0"]
 }
--- edbterraform/data/terraform/aws/modules/security/outputs.tf
+++ edbterraform/data/terraform/aws/modules/security/outputs.tf
@@ -1,6 +1,3 @@
 output "security_group_ids" {
-  value = flatten([aws_security_group.OUTBOUND_ACCESS.id ,[
-    for key, values in aws_security_group.rules:
-      values.id
-  ]])
+  value = [aws_security_group.rules.id]
 }
