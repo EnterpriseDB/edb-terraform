400bac0bb1186a89150860e9fdfcfa5d372c1d91
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-03-28 18:47:21 +0000

    update security rule to handle egress and ingress

--- edbterraform/data/terraform/aws/modules/security/main.tf
+++ edbterraform/data/terraform/aws/modules/security/main.tf
@@ -7,27 +7,21 @@ resource "aws_security_group" "rules" {
   }
 }
 
-resource "aws_security_group_rule" "ingress" {
+resource "aws_security_group_rule" "rule" {
   for_each = local.merged_rules
   security_group_id = aws_security_group.rules.id
   description = each.value.description
-  type = "ingress"
+  type = each.value.type
 
   from_port   = each.value.protocol == "icmp" ? 8 : each.value.port != null ? each.value.port : -1
   to_port     = each.value.port != null && each.value.protocol != "icmp"  ? each.value.port : -1
   protocol    = each.value.protocol
-  cidr_blocks = each.value.ingress_cidrs != null ? each.value.ingress_cidrs : var.ingress_cidrs
-}
+  cidr_blocks = each.value.cidrs
 
-# Default is removed from VPC when this resource is used
-# Manually add it back to allow instances to have outbound access
-# Should be moved to network tags so machines can specify if they need public access
-resource "aws_security_group_rule" "OUTBOUND_ACCESS" {
-  security_group_id = aws_security_group.rules.id
-  description = "OUTBOUND access"
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = -1
-  cidr_blocks = ["0.0.0.0/0"]
+  lifecycle {
+    precondition {
+      condition     = each.value.type == "ingress" || each.value.type == "egress"
+      error_message = "${each.key} has type ${each.type}. Must be ingress or egress."
+    }
+  }
 }
