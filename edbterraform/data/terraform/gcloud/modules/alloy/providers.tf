b0c4d5010148e3848dfd28d1e1d9a452d45192c2
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-07-13 12:19:08 +0000

    gcloud-alloydb - use of general availability instead of beta provider.   * GA APIs announced: 2022-12-14   * hashicorp/google terraform release: v4.53.0

--- edbterraform/data/terraform/gcloud/modules/alloy/providers.tf
+++ edbterraform/data/terraform/gcloud/modules/alloy/providers.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google-beta"
-      version = ">= 4.46.0"
+      source  = "hashicorp/google"
+      version = ">= 4.53.0"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.3.6"
 }
