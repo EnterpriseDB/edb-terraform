5b87bd228dc58062ec8d831a7ba2382a6f068361
Bryan Barajas <bryan.barajas@enterprisedb.com>
2023-08-09 21:23:49 +0000

    GCP BigAnimal Module - requires BigAnimal Provider v0.5.1(initial support)

---
+++ edbterraform/data/terraform/gcloud/modules/biganimal/main.tf
@@ -0,0 +1,44 @@
+resource "biganimal_cluster" "instance" {
+
+    # required 
+    cloud_provider = local.cloud_provider
+    cluster_architecture {
+        id = var.cluster_type
+        nodes = var.node_count
+    }
+    cluster_name = local.cluster_name
+    instance_type = local.instance_type
+    password = var.password
+    pg_type = var.engine
+    pg_version = var.engine_version
+    project_id = var.project.id
+    region = var.region
+    storage {
+        volume_type = var.volume.type
+        volume_properties = var.volume.properties
+        size = local.volume_size
+        # IOPs and Throughput not configurable for pd-ssd
+    }
+
+    # optional
+    dynamic "allowed_ip_ranges" {
+        for_each = { for key,values in var.allowed_ip_ranges: key=>values }
+        content {
+            cidr_block = allowed_ip_ranges.value.cidr_block
+            description = allowed_ip_ranges.value.description
+        }
+    }
+    # backup_retention_period = "30d"
+    csp_auth = false
+    dynamic "pg_config" {
+        for_each = { for key,values in var.settings: key=>values }
+        content {
+            name         = pg_config.value.name
+            value        = pg_config.value.value
+        }
+    }
+    # VPC Peering or other must be configured with CLI
+    # when using a private network
+    private_networking = !var.publicly_accessible
+    read_only_connections = false
+}
--- edbterraform/data/terraform/gcloud/modules/specification/outputs.tf
+++ edbterraform/data/terraform/gcloud/modules/specification/outputs.tf
@@ -87,6 +87,20 @@ output "region_alloys" {
   }
 }
 
+output "region_biganimals" {
+  value = {
+    for name, biganimal_spec in var.spec.biganimal : biganimal_spec.region => {
+      name = name
+      spec = merge(biganimal_spec, {
+        # spec project tags
+        tags = merge(local.tags, biganimal_spec.tags, {
+          Name = format("%s-%s-%s", name, local.cluster_name, random_id.apply.id)
+        })
+      })
+    }...
+  }
+}
+
 output "region_kubernetes" {
   value = {
     for name, spec in var.spec.kubernetes : spec.region => {
