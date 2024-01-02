90ec997875ab0949a126018bb68b28b28b3cc949
Bryan Barajas <bryan.barajas@enterprisedb.com>
2024-01-02 19:09:45 +0000

    Azure BigAnimal Module changes: - Use biganimal api to setup wal storage - Set superuser access
    Related AWS PR: https://github.com/EnterpriseDB/edb-terraform/pull/103
    Commit start: 0d5c38097d76d3b9024285bb6b8cc546c14c4153
    Commit end: 8044f6fbfa48e96ff0abd46139c392d898b10d21

--- edbterraform/data/terraform/azure/modules/biganimal/providers.tf
+++ edbterraform/data/terraform/azure/modules/biganimal/providers.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     biganimal = {
       source  = "EnterpriseDB/biganimal"
-      version = ">= 0.5.1"
+      version = "~> 0.6.0"
+    }
+    toolbox = {
+      source = "bryan-bar/toolbox"
+      version = "~> 0.2.2"
     }
   }
 }
