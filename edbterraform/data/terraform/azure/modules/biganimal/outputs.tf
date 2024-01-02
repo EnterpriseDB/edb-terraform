5bcd5712b768d2bd69c85ee10ffb3a7ad9c6bd69
Bryan Barajas <bryan.barajas@enterprisedb.com>
2024-01-02 19:09:45 +0000

    Azure BigAnimal Module changes: - Use biganimal api to setup wal storage - Set superuser access
    Related AWS PR: https://github.com/EnterpriseDB/edb-terraform/pull/103
    Commit start: 0d5c38097d76d3b9024285bb6b8cc546c14c4153
    Commit end: 8044f6fbfa48e96ff0abd46139c392d898b10d21

--- edbterraform/data/terraform/azure/modules/biganimal/outputs.tf
+++ edbterraform/data/terraform/azure/modules/biganimal/outputs.tf
@@ -2,7 +2,7 @@ locals {
     // https://github.com/hashicorp/terraform/issues/23893#issuecomment-577963377
     // https://datatracker.ietf.org/doc/html/rfc3986#appendix-B
     pattern = "(?:(?P<scheme>[^:/?#]+):)?(?://(?P<authority>[^/?#]*))?(?P<path>[^?#]*)(?:\\?(?P<query>[^#]*))?(?:#(?P<fragment>.*))?"
-    uri_split = regex(local.pattern, biganimal_cluster.instance.connection_uri)
+    uri_split = regex(local.pattern, try(local.cluster_output.connection.pgUri, local.cluster_output.connection_uri))
     username = split("@", local.uri_split.authority)[0]
     port = split(":", local.uri_split.authority)[1]
     domain = trimsuffix(trimprefix(local.uri_split.authority, "${local.username}@"), ":${local.port}")
@@ -10,31 +10,32 @@ locals {
 }
 
 output "project_id" {
-    value = biganimal_cluster.instance.project_id
+    value = var.project.id
 }
 
 output "cluster_id" {
-    value = biganimal_cluster.instance.cluster_id
+    value = local.cluster_id
 }
 
 output "cloud_provider" {
-  value = biganimal_cluster.instance.cloud_provider
+  value = try(local.cluster_output.provider.cloudProviderId, local.cluster_output.cloud_provider)
 }
 
 output "cluster_name" {
-    value = biganimal_cluster.instance.cluster_name
+  value = try(local.cluster_output.clusterName, local.cluster_output.cluster_name)
 }
 
 output "cluster_type" {
-    value = biganimal_cluster.instance.cluster_type
+  value = try(local.cluster_output.clusterType, local.cluster_output.cluster_type)
 }
 
+
 output "cluster_architecture" {
-    value = biganimal_cluster.instance.cluster_architecture
+    value = try(local.cluster_output.clusterArchitecture, local.cluster_output.cluster_architecture)
 }
 
 output "region" {
-  value = biganimal_cluster.instance.region
+  value = local.cluster_region
 }
 
 output "dbname" {
@@ -47,7 +48,7 @@ output "username" {
 
 output "password" {
   sensitive = true
-  value = biganimal_cluster.instance.password
+  value = var.password
 }
 
 output "port" {
@@ -55,37 +56,68 @@ output "port" {
 }
 
 output "private_ip" {
-  value = biganimal_cluster.instance.private_networking ? local.domain : null
+  value = try(local.cluster_output.privateNetworking ,local.cluster_output.private_networking) ? local.domain : null
 }
 
 output "public_ip" {
-  value = biganimal_cluster.instance.private_networking ? null : local.domain
+  value = try(local.cluster_output.privateNetworking ,local.cluster_output.private_networking) ? null : local.domain
 }
 
 output "engine" {
-  value = biganimal_cluster.instance.pg_type
+  value = try(local.cluster_output.pgType.pgTypeId, local.cluster_output.pg_type)
 }
 
 output "version" {
-  value = biganimal_cluster.instance.pg_version
+  value = try(local.cluster_output.pgVersion.pgVersionId ,local.cluster_output.pg_version)
 }
 
 output "instance_type" {
-  value = biganimal_cluster.instance.instance_type
+  value = try(local.cluster_output.instanceType.instanceTypeId, local.cluster_output.instance_type)
 }
 
 output "connection_uri" {
-  value = biganimal_cluster.instance.connection_uri
+  value = try(local.cluster_output.connection.pgUri, local.cluster_output.connection_uri)
 }
 
 output "read_only_uri" {
-    value = biganimal_cluster.instance.ro_connection_uri
+    value = try(local.cluster_output.connection.readOnlyPgUri, local.cluster_output.ro_connection_uri)
 }
 
 output "tags" {
   value = var.tags
 }
 
+output "vpc_name" {
+  value = var.cloud_account ? local.vpc_name : ""
+}
+
+output "buckets" {
+  value = var.cloud_account ? {
+    postgres = {
+      bucket = local.postgres_bucket
+      prefix = local.postgres_bucket_prefix
+    }
+    container = {
+      bucket = local.container_bucket
+      prefix = local.partial_container_prefix
+    }
+    metrics = {
+      bucket = local.metrics_bucket
+    }
+  } : {}
+}
+
 output "all" {
-    value = biganimal_cluster.instance
+    value = local.cluster_output
+}
+
+output "resources" {
+  value = {
+    biganimal_cluster = {
+      instance = biganimal_cluster.instance
+    }
+    toolbox_external = {
+      api = toolbox_external.api
+    }
+  }
 }
--- edbterraform/data/terraform/azure/modules/biganimal/variables.tf
+++ edbterraform/data/terraform/azure/modules/biganimal/variables.tf
@@ -49,6 +49,10 @@ variable "engine_version" {
 }
 variable "settings" {}
 variable "volume" {}
+variable "wal_volume" {
+  nullable = false
+  default = {}
+}
 variable "password" {}
 
 variable "publicly_accessible" {
@@ -82,4 +86,55 @@ locals {
 
   cloud_provider = var.cloud_account ? "azure" : "bah:azure"
   cluster_name = format("%s-%s", var.name, var.name_id)
+
+  // Create an object that excludes any null objects
+  TERRAFORM_API_MAPPING = {
+    iops = "iops"
+    throughput = "throughput"
+    size_gb = "size"
+    properties = "volumePropertiesId"
+    type = "volumeTypeId"
+  }
+  // Remove null values from the volume properties and save with the api variable naming as the key
+  // Size must be saved as a string and with the Gi suffix
+  API_DATA = {
+    clusterName = local.cluster_name
+    instanceType = { instanceTypeId = local.instance_type }
+    allowedIpRanges = [
+      for key, value in var.allowed_ip_ranges :
+        {
+          cidrBlock = value.cidr_block
+          description = value.description
+        }
+    ]
+    # "premiumstorage" requires "azure" as a prefix when calling the api directly
+    storage = {
+      for key, value in var.volume : local.TERRAFORM_API_MAPPING[key] =>
+        key == "size_gb" ? "${value} Gi"
+        : contains(["type"], key) && contains(["premiumstorage"], value) ? "azurepremiumstorage"
+        : tostring(value) if value != null
+    }
+    walStorage = {
+      for key, value in var.wal_volume : local.TERRAFORM_API_MAPPING[key] =>
+        key == "size_gb" ? "${value} Gi"
+        : contains(["type"], key) && contains(["premiumstorage"], value) ? "azurepremiumstorage"
+        : tostring(value) if value != null
+    }
+    # required 
+    provider = { cloudProviderId = local.cloud_provider }
+    clusterArchitecture = {
+        clusterArchitectureId = var.cluster_type
+        nodes = var.node_count
+    }
+    region = { regionId = var.region }
+    pgVersion = { pgVersionId = tostring(var.engine_version) }
+    pgType = { pgTypeId = var.engine }
+    pgConfig = var.settings
+    password = var.password
+    privateNetworking = !var.publicly_accessible
+    backupRetentionPeriod = "1d"
+    cspAuth = false
+    readOnlyConnections = false
+    superuserAccess = true
+  }
 }
