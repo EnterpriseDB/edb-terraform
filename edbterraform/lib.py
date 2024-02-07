5041030893474230f10d1354fc4a6123b95ef0ea
Bryan Barajas <bryan.barajas@enterprisedb.com>
2024-02-07 10:06:04 +0000

    terraform templates/modules - move common variables under a shared file

--- edbterraform/lib.py
+++ edbterraform/lib.py
@@ -172,6 +172,7 @@ def create_project_dir(
     SCRIPT_DIRECTORY = Path(__file__).parent.resolve()
     TERRAFORM_MODULES_DIRECTORY = SCRIPT_DIRECTORY / 'data' / 'terraform' / cloud_service_provider
     TERRAFORM_PROVIDERS_FILE = SCRIPT_DIRECTORY / 'data' / 'terraform' / 'providers.tf.json'
+    TERRAFORM_COMMON_VARS_FILE = SCRIPT_DIRECTORY / 'data' / 'terraform' / 'common_vars.tf'
     TERRAFORM_VERSIONS_FILE = SCRIPT_DIRECTORY / 'data' / 'terraform' / 'versions.tf'
     TERRAFORM_STATE_FILE = project_directory / 'terraform.tfstate'
     PROJECT_PATH_PERMISSIONS = 0o750
@@ -193,6 +194,7 @@ def create_project_dir(
         shutil.copytree(TERRAFORM_MODULES_DIRECTORY, project_directory)
         shutil.copyfile(TERRAFORM_PROVIDERS_FILE, project_directory / TERRAFORM_PROVIDERS_FILE.name)
         shutil.copyfile(TERRAFORM_VERSIONS_FILE, project_directory / TERRAFORM_VERSIONS_FILE.name)
+        shutil.copyfile(TERRAFORM_COMMON_VARS_FILE, project_directory / TERRAFORM_COMMON_VARS_FILE.name)
         os.chmod(project_directory, PROJECT_PATH_PERMISSIONS)
 
         # Create the statefile and change file/folder permissions 
