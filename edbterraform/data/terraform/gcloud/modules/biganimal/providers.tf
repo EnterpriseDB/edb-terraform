3e9b7be6da7a3ff342b33e8cd6dd17959972a538
Bryan Barajas <bryan.barajas@enterprisedb.com>
2024-02-13 00:39:57 +0000

    Biganimal module - release 0.7.1 fixes issue 455 where unexpected plan results caused terraform to fail after resources were successfully created. Ref: https://github.com/EnterpriseDB/terraform-provider-biganimal/issues/455

--- edbterraform/data/terraform/gcloud/modules/biganimal/providers.tf
+++ edbterraform/data/terraform/gcloud/modules/biganimal/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     biganimal = {
       source  = "EnterpriseDB/biganimal"
-      version = "~> 0.6.0"
+      version = "~> 0.7.1"
     }
     toolbox = {
       source = "bryan-bar/toolbox"
--- edbterraform/data/terraform/versions.tf
+++ edbterraform/data/terraform/versions.tf
@@ -7,7 +7,7 @@ terraform {
 
     biganimal = {
       source = "registry.terraform.io/EnterpriseDB/biganimal"
-      version = "<= 0.6.1"
+      version = "<= 0.7.1"
     }
     # https://github.com/EnterpriseDB/terraform-provider-toolbox/issues/44
     toolbox = {
