e5e686c8f2a3c5ac60199a7bec50f604557ab986
Bryan Barajas <bryan.barajas@enterprisedb.com>
2024-02-14 14:13:02 +0000

    terraform root module - force_dynamic_ip variable added so that a dynamic ip may be added to the service_cidrblocks. 0.0.0.0/0 will no longer be set a default to avoid accidental exposure of resources.

--- edbterraform/data/terraform/common_vars.tf
+++ edbterraform/data/terraform/common_vars.tf
@@ -18,5 +18,48 @@ variable "public_cidrblock" {
 variable "service_cidrblocks" {
   description = "Default cidr blocks for service ports"
   type = list(string)
-  default = ["0.0.0.0/0"]
+  default = []
+}
+
+variable "force_dynamic_ip" {
+  description = "Force the use of a dynamic IP address which will be appended to service_cidrblocks"
+  type = bool
+  default = false
+}
+
+variable "dynamic_service_ip_mask" {
+  type = number
+  default = 32
+  nullable = false
+}
+
+variable "dynamic_service_ip_url" {
+  type = string
+  default = "https://ipinfo.io/ip"
+  nullable = false
+}
+
+# Keep at the root level so that it is always a known value.
+# Data sources within modules are not computed until the module is instantiated,
+#  which causes for_each loops to fail since it is an unknown computed value.
+data "http" "instance_ip" {
+  count = var.force_dynamic_ip ? 1 : 0
+  url = var.dynamic_service_ip_url
+  request_headers = {
+    Accept = "application/text"
+  }
+}
+
+locals {
+  # format the ip with the mask to get a valid cidr block
+  # ex: cidrhost("1.2.3.4/32",0) => 1.2.3.4 | cidrhost("1.2.3.4/24",0) => 1.2.3.0 | cidrhost("1.2.3.4/16",0) => 1.2.0.0 | cidrhost("1.2.3.4/32",0) => 1.0.0.0
+  dynamic_ip = var.force_dynamic_ip ? [
+    "${cidrhost(
+        format("%s/%s",
+          split("/", data.http.instance_ip[0].response_body)[0], # Drop any prefined masks
+          var.dynamic_service_ip_mask),
+        0)
+    }/${var.dynamic_service_ip_mask}"
+  ] : []
+  service_cidrblocks = concat(var.service_cidrblocks, local.dynamic_ip)
 }
