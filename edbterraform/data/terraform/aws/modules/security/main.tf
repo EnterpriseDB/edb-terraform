26f49140dc569cc5c85190a7cf87ea3b57c488b8
Bryan Barajas <bryan.barajas@enterprisedb.com>
2024-02-21 09:07:56 +0000

    AWS Security module - use the new ingress and egress resource. It still creates duplicate rules but this is the recommended resource over security group rules inline or as a resource.
    Ref: f37054fa8fb8f3981b558d1e9a4e68938a89d080

--- edbterraform/data/terraform/aws/modules/security/main.tf
+++ edbterraform/data/terraform/aws/modules/security/main.tf
@@ -7,23 +7,62 @@ resource "aws_security_group" "rules" {
   }, var.tags)
 }
 
-resource "aws_security_group_rule" "rule" {
-  for_each = local.rules
+resource "aws_vpc_security_group_ingress_rule" "in" {
+  for_each = { for name, rule in local.rules : name => rule if rule.type == "ingress" }
+
   security_group_id = aws_security_group.rules.id
   description = each.value.description
-  type = each.value.type
 
   from_port   = each.value.protocol == "icmp" ? 8 : each.value.port != null ? each.value.port : -1
   to_port     = (
-      each.value.to_port != null && each.value.protocol != "icmp"  ? each.value.to_port : 
+      each.value.to_port != null && each.value.protocol != "icmp"  ? each.value.to_port :
       each.value.port != null && each.value.protocol != "icmp" ? each.value.port : -1
     )
-  protocol    = each.value.protocol
-  cidr_blocks = each.value.cidrs
+  ip_protocol = each.value.protocol
+  cidr_ipv4   = one(each.value.cidrs)
 
   lifecycle {
     precondition {
-      condition     = each.value.type == "ingress" || each.value.type == "egress"
+      condition     = each.value.type == "ingress"
+      error_message = "${each.key} has type ${each.value.type}. Must be ingress or egress."
+    }
+
+    precondition {
+      error_message = "port defaults must be one of: service, public, internal or an empty string ('')"
+      condition = contains(["service", "internal", "public", ""], try(each.value.defaults, ""))
+    }
+
+    precondition {
+      condition     = each.value.cidrs != null && length(each.value.cidrs) > 0
+      error_message = <<-EOT
+        Cidr blocks cannot be an empty list.
+        - AWS Security groups are an allow list meaning an empty list is the same as not having the rule itself.
+        - When defaults is set to 'service' and has no cidrs defined, it is removed from the list to avoid creation.
+        The AWS API will return the following error after 5-10 minutes:
+          Error: waiting for Security Group (sg-xxxx) Rule (sgrule-xxxx) create: couldn't find resource
+        Rule: ${jsonencode(each.value)}
+      EOT
+    }
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "out" {
+  for_each = { for name, rule in local.rules : name => rule if rule.type == "egress" }
+
+  security_group_id = aws_security_group.rules.id
+  description = each.value.description
+
+  from_port   = each.value.protocol == "icmp" ? 8 : each.value.port != null ? each.value.port : -1
+  to_port     = (
+      each.value.to_port != null && each.value.protocol != "icmp"  ? each.value.to_port :
+      each.value.port != null && each.value.protocol != "icmp" ? each.value.port : -1
+    )
+  ip_protocol = each.value.protocol
+  cidr_ipv4   = one(each.value.cidrs)
+
+  lifecycle {
+    precondition {
+      condition     = each.value.type == "egress"
       error_message = "${each.key} has type ${each.value.type}. Must be ingress or egress."
     }
 
