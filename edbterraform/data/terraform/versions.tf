fca1fafb0f046ff3c4c48ff27fa3e6fea40e1b01
Bryan Barajas <bryan.barajas@enterprisedb.com>
2024-05-14 21:29:56 +0000

    terraform dependencies - FIX - use less-than, '<', for use with dependabot
    Dependabot will pick a non-existing version when using less-than or
    equals, '<=', as a version constraint.
    
    Ref: https://github.com/dependabot/dependabot-core/issues/8959

--- edbterraform/data/terraform/versions.tf
+++ edbterraform/data/terraform/versions.tf
@@ -7,57 +7,57 @@ terraform {
 
     biganimal = {
       source = "registry.terraform.io/EnterpriseDB/biganimal"
-      version = "<= 0.8.1"
+      version = "< 0.8.2"
     }
     # https://github.com/EnterpriseDB/terraform-provider-toolbox/issues/44
     toolbox = {
       source = "registry.terraform.io/bryan-bar/toolbox"
-      version = "<= 0.2.2"
+      version = "< 0.2.3"
     }
 
     aws = {
       source = "registry.terraform.io/hashicorp/aws"
-      version = "<= 5.34.0"
+      version = "< 5.34.1"
     }
 
     azurerm = {
       source = "registry.terraform.io/hashicorp/azurerm"
-      version = "<= 3.89.0"
+      version = "< 3.89.1"
     }
 
     google = {
       source = "registry.terraform.io/hashicorp/google"
-      version = "<= 5.14.0"
+      version = "< 5.14.1"
     }
 
     kubernetes = {
       source = "registry.terraform.io/hashicorp/kubernetes"
-      version = "<= 2.25.2"
+      version = "< 2.25.3"
     }
 
     null = {
       source = "registry.terraform.io/hashicorp/null"
-      version = "<= 3.2.2"
+      version = "< 3.2.3"
     }
 
     time = {
       source = "registry.terraform.io/hashicorp/time"
-      version = "<= 0.10.0"
+      version = "< 0.10.1"
     }
 
     local = {
       source = "registry.terraform.io/hashicorp/local"
-      version = "<= 2.4.1"
+      version = "< 2.4.2"
     }
 
     random = {
       source = "registry.terraform.io/hashicorp/random"
-      version = "<= 3.6.0"
+      version = "< 3.6.1"
     }
 
     tls = {
       source = "registry.terraform.io/hashicorp/tls"
-      version = "<= 4.0.5"
+      version = "< 4.0.6"
     }
 
   }
