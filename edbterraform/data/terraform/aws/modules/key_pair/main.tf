c9dced74ff2e713ebd428f31749ae18df9fdb686
Bryan Barajas <bryan.barajas@enterprisedb.com>
2024-08-22 05:44:39 +0000

    AWS ssh key module - set tags

--- edbterraform/data/terraform/aws/modules/key_pair/main.tf
+++ edbterraform/data/terraform/aws/modules/key_pair/main.tf
@@ -1,6 +1,7 @@
 variable "ssh_pub_key" {}
 variable "key_name" {}
 variable "name_id" { default = "0" }
+variable "tags" {}
 
 terraform {
   required_providers {
@@ -13,4 +14,5 @@ terraform {
 resource "aws_key_pair" "key_pair" {
   key_name   = "${var.key_name}-${var.name_id}"
   public_key = var.ssh_pub_key
+  tags       = var.tags
 }
