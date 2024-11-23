b7a0fcdafdbe991a65b5f2978ad810a4fa46ea3c
Bryan Barajas <bryan.barajas@enterprisedb.com>
2024-11-23 13:29:09 +0000

    AWS Kubernetes module - additional outputs added: raw, cluster_info, cluster_auth

--- edbterraform/data/terraform/aws/modules/kubernetes/outputs.tf
+++ edbterraform/data/terraform/aws/modules/kubernetes/outputs.tf
@@ -1,3 +1,8 @@
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
+}
+
 output "cluster_id" {
   description = "EKS cluster ID - only available for local Amazon EKS cluster on the AWS Outpost. Not available for an AWS EKS cluster on AWS cloud"
   value       = module.eks.cluster_id
@@ -23,8 +28,17 @@ output "region" {
   value       = var.region
 }
 
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = module.eks.cluser_name
+output "raw" {
+  description = "Raw EKS cluster output"
+  value       = module.eks
 }
 
+output "cluster_info" {
+  description = "Cluster general information"
+  value       = data.aws_eks_cluster.cluster
+}
+
+output "cluster_auth" {
+  description = "Cluster authentication information"
+  value       = data.aws_eks_cluster_auth.cluster
+}
