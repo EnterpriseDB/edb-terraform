b01283e603335187adac53c4f8e5eb47d77d8d5a
Bryan Barajas <bryan.barajas@enterprisedb.com>
2024-11-23 13:24:26 +0000

    FIX - AWS Kubernetes module - use cluster name instead of cluster id as it is only available for local EKS clusters on AWS Outpost

--- edbterraform/data/terraform/aws/modules/kubernetes/main.tf
+++ edbterraform/data/terraform/aws/modules/kubernetes/main.tf
@@ -1,11 +1,11 @@
 data "aws_availability_zones" "available" {}
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 module "vpc" {
